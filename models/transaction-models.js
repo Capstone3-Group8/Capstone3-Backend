cbaad719b938c67141922474b34039e771181659
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-08-12 14:11:41 -0400

    Add Transaction button fully functional

--- models/transaction-models.js
+++ models/transaction-models.js
@@ -14,7 +14,7 @@ const Transaction = db.define("Transaction", {
   },
   account_id: {
     type: DataTypes.INTEGER,
-    allowNull: false,
+    allowNull: true,
   },
   category_id: {
     type: DataTypes.INTEGER,
