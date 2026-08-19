d19fcd89923eb1553a62a72c21b450c2799f976a
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-08-19 11:10:11 -0400

    simple commit

--- routes/plaid.routes.js
+++ routes/plaid.routes.js
@@ -3,27 +3,6 @@ const router = express.Router();
 const { plaidClient } = require("../plaid");
 const { requireAuth } = require("../middleware/auth");
 const { PlaidItem, PlaidAccount, PlaidTransaction } = require("../models");
-<<<<<<< HEAD
-
-router.post('/create-link-token', requireAuth, async(req,res,next) => {
-    try {
-        //linkTokenCreate() is a call over the network to Plaid's server so returns a promise
-        const response = await plaidClient.linkTokenCreate({ 
-            user: {
-                client_user_id: req.user.id,
-            }, 
-            client_name: 'Capstone3', 
-            products: process.env.PLAID_PRODUCTS.split(','),
-            country_codes: process.env.PLAID_COUNTRY_CODES.split('s,'),
-            language: 'en'
-        });
-        res.json({link_token: response.data.link_token})
-    } catch (error) {
-        console.error('Plaid error:', error.response?.data || error.message )
-        next(error)
-    }
-=======
->>>>>>> main
 
 router.post("/create-link-token", requireAuth, async (req, res, next) => {
   try {
@@ -87,48 +66,6 @@ router.post("/exchange-public-token", requireAuth, async (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
-router.post('/sync-transactions', requireAuth, async(req, res, next) => {
-    try {
-        const plaidItem = await PlaidItem.findOne({ where: { user_id: req.user.id }})
-        if (!plaidItem){
-            return res.status(404).json({ error: 'No linked bank account found' })
-        }
-        
-        const endDate = new Date().toISOString().split('T')[0]; // today, as YYYY-MM-DD
-        const startDate = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString().split('T')[0]; // 30 days ago
-
-        const transactionResponse = await plaidClient.transactionsGet({ 
-            access_token: plaidItem.access_token,
-            start_date: startDate,
-            end_date: endDate
-        })
-
-        const transactions = transactionResponse.data.transactions;
-
-        await Promise.all(
-            transactions.map((transaction) =>
-            PlaidTransaction.create({
-                user_id: req.user.id,
-                account_id: transaction.account_id,
-                transaction_id: transaction.transaction_id,
-                amount: transaction.amount,
-                date: transaction.date,
-                name: transaction.name,
-                merchant_name: transaction.merchant_name,
-                // categories are arrays ofr something like["Food and Drink, Restaurants"] so
-                // just turn these arrays into strings to avoid errors when creating them into postgres
-                category: transaction.category ? transaction.category.join(', ') : null,
-                pending: transaction.pending
-            }))
-        )
-
-        res.json({ success: true, count: transactions.length });
-
-
-    } catch (error) {
-        next(error)
-=======
 router.post("/sync-transactions", requireAuth, async (req, res, next) => {
   try {
     const plaidItem = await PlaidItem.findOne({
@@ -136,7 +73,6 @@ router.post("/sync-transactions", requireAuth, async (req, res, next) => {
     });
     if (!plaidItem) {
       return res.status(404).json({ error: "No linked bank account found" });
->>>>>>> main
     }
 
     const endDate = new Date().toISOString().split("T")[0]; // today, as YYYY-MM-DD
